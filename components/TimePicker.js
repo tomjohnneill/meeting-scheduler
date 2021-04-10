37a58bc72025eefdb9f796b7844261f7bd89f706
tomjohnneill <tomjohnneill@live.co.uk>
2021-04-10 22:55:51 +0100

    time picker works better

--- components/TimePicker.js
+++ components/TimePicker.js
@@ -1,38 +1,51 @@
 import dayjs from "dayjs";
+import duration from "dayjs/plugin/duration";
 import { useEffect, useRef, createRef, useState } from "react";
 
-const times = [];
-for (let i = 0; i < 96; i += 1) {
-  times.push(
-    dayjs()
-      .hour(0)
-      .minute(0)
-      .second(0)
-      .millisecond(0)
-      .add(15 * i, "minutes")
-  );
-}
+dayjs.extend(duration);
 
-const getMatchingTime = () => {
-  const now = dayjs();
-  for (let i = 0; i < 96; i += 1) {
-    if (now < times[i]) {
-      return i;
-    }
+const TimePicker = ({ onChange, from, placeholder, className, step }) => {
+  const stepCount = (24 * 60) / step;
+
+  const times = [];
+  for (let i = 0; i < stepCount; i += 1) {
+    times.push(
+      dayjs()
+        .hour(0)
+        .minute(0)
+        .second(0)
+        .millisecond(0)
+        .add(step * i, "minutes")
+    );
   }
-  return 96;
-};
 
-const matchTimeString = (typed) => {
-  for (let i = 0; i < 96; i += 1) {
-    if (times[i].format("HH:mm").slice(0, typed.length) === typed) {
-      return i;
+  const getMatchingTime = () => {
+    const now = dayjs();
+    for (let i = 0; i < stepCount; i += 1) {
+      if (now < times[i]) {
+        return i;
+      }
     }
-  }
-  return null;
-};
+  };
 
-const TimePicker = ({ onChange }) => {
+  const matchTimeString = (typed) => {
+    for (let i = 0; i < stepCount; i += 1) {
+      if (times[i].format("HH:mm").slice(0, typed.length) === typed) {
+        return i;
+      }
+    }
+    return null;
+  };
+
+  const timeString = (a, b) => {
+    const hourDiff = a.diff(b, "hours");
+    if (hourDiff < 1) {
+      return `(${dayjs.duration(a.diff(b)).format("mm")} mins)`;
+    } else {
+      return `(${dayjs.duration(a.diff(b)).format("H")} hrs)`;
+    }
+  };
+
   const dropdownEl = useRef(null);
 
   const [selected, setSelected] = useState(null);
@@ -42,7 +55,7 @@ const TimePicker = ({ onChange }) => {
       const { scrollHeight } = dropdownEl.current;
 
       dropdownEl.current.scrollTop =
-        (((selected - 2) * 1.0) / 96) * scrollHeight;
+        (((selected - 2) * 1.0) / stepCount) * scrollHeight;
     }
   }, [selected]);
 
@@ -81,7 +94,7 @@ const TimePicker = ({ onChange }) => {
   };
 
   return (
-    <div className="relative inline-block text-left">
+    <div className={`relative inline-block text-left ${className}`}>
       <div>
         <input
           onBlur={() => {
@@ -90,11 +103,11 @@ const TimePicker = ({ onChange }) => {
             }
             setTimeout(() => {
               setDropdownOpen(false);
-            }, 200);
+            }, 300);
           }}
           onKeyDown={handleKeyDown}
           onFocus={() => setDropdownOpen(true)}
-          placeholder="Time"
+          placeholder={placeholder || "Time"}
           ref={inputEl}
           className="inline-flex w-32 justify-center ounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
           id="options-menu"
@@ -117,7 +130,9 @@ To: "transform opacity-0 scale-95"
       {dropdownOpen && (
         <div
           ref={dropdownEl}
-          className={`max-h-80 transition-all overflow-y-auto origin-top-left absolute left-0 mt-2 w-32 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none
+          className={`max-h-80 z-50 transition-all overflow-y-auto origin-top-left absolute left-0 mt-2 ${
+            from ? "w-40" : "w-32"
+          } rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none
             ${dropdownOpen ? "opacity-100" : "opacity-0"}
           `}
           role="menu"
@@ -128,16 +143,15 @@ To: "transform opacity-0 scale-95"
             {times.map((time, i) => (
               <a
                 onClick={(e) => {
-                  //setSelected(i);
-                  console.log({ i });
                   setTyped(times[i].format("HH:mm"));
+                  onChange(times[i]);
                 }}
                 className={`cursor-pointer block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 focus:bg-gray-100 hover:text-gray-900 ${
                   selected === i ? "bg-gray-100" : ""
                 }`}
                 role="menuitem"
               >
-                {time.format("HH:mm")}
+                {time.format("HH:mm")} {from && timeString(time, from)}
               </a>
             ))}
           </div>
--- pages/create.js
+++ pages/create.js
@@ -4,9 +4,29 @@ import TimePicker from "../components/TimePicker";
 function MyApp() {
   const [value, onChange] = useState(new Date());
 
+  const [start, setStart] = useState(null);
+  const [end, setEnd] = useState(null);
+
   return (
-    <div className="w-full h-screen flex items-center justify-center flex-col">
-      <TimePicker />
+    <div className="w-full h-screen flex items-center justify-center">
+      <div>
+        <p>From</p>
+        <TimePicker
+          step={15}
+          placeholder="Start"
+          onChange={(value) => setStart(value)}
+          className="mr-2"
+        />
+      </div>
+      <div>
+        <p>To</p>
+        <TimePicker
+          step={30}
+          placeholder="End"
+          onChange={(value) => setEnd(value)}
+          from={start}
+        />
+      </div>
     </div>
   );
 }
