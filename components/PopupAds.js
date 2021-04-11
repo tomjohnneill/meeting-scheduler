e4a23ff31904841ec37049bc0631eecff4a2103b
Reda <redahaq@gmail.com>
2021-04-12 00:56:06 +0100

    feat(add handleclick yes, adjust ad styles)

--- components/PopupAds.js
+++ components/PopupAds.js
@@ -20,34 +20,24 @@ const PopupAds = ({ handleClick }) => {
   const ads = [
     {
       title: 'Congratulations!!!!!',
-      titleStyle: '',
+      titleStyle: 'font-bold',
       image:
         'https://cdn.vox-cdn.com/thumbor/XKrMqqGnvhJqbwt9DvTjGDPcG7M=/1600x0/filters:no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/22341197/doge.gif',
-      body: "You're our 5000th visitor! You WIN 5M DogeCoin",
+      body: "You're our 5000th visitor! You WIN 50,000M DogeCoin",
       buttonText: 'CLICK TO CLAIM YOUR PRIZE',
       buttonColor: 'bg-green-500',
-      bgColor: 'bg-blue-900',
+      bar: 'bg-blue-900 h-7',
       animation: 'flashy',
       border: 'border border-blue-900',
     },
     {
-      title: "You're a winner!!!",
+      title: '60% of the time, it works every time',
       titleStyle: 'text-red-500 font-bold',
-      image:
-        'https://www.teads.com/wp-content/uploads/2014/07/Save-Puppy-YouTube-Ad.png',
-      buttonText: 'CLICK TO CLAIM YOUR PRIZE',
-      buttonColor: 'bg-red-500',
-      bgColor: 'bg-gray-600',
+      image: '/ads/sexpanther.png',
+      buttonText: 'ORDER YOURS TODAY',
+      buttonColor: 'bg-red-500 flashy',
+      bar: 'bg-red-500 h-7',
       border: 'border-4 border-red-500 border-dashed',
-    },
-    {
-      title: "You're a winner!!!",
-      titleStyle: 'text-red-500 font-bold',
-      image: '/galaxy/Slice 1.png',
-      buttonText: 'CLICK TO CLAIM YOUR PRIZE',
-      buttonColor: 'bg-yellow-300',
-      bgColor: 'bg-gray-400',
-      border: 'border-2 border-blue-500',
     },
   ];
 
@@ -55,9 +45,9 @@ const PopupAds = ({ handleClick }) => {
     return (
       <>
         <div
-          className={`shadow-xl absolute right-0 mr-12 w-64 z-40 flex flex-col justify-center bg-white ${ads[popUp].border}`}
+          className={`shadow-xl absolute right-0 mr-16 w-64 z-40 flex flex-col justify-center bg-white ${ads[popUp].border}`}
         >
-          <div className={`${ads[popUp].bgColor} h-6 flex justify-end`}>
+          <div className={`${ads[popUp].bar} flex justify-end`}>
             <button className="absolute right-0 mr-1 text-white">x</button>
           </div>
           <h2
@@ -65,7 +55,7 @@ const PopupAds = ({ handleClick }) => {
           >
             {ads[popUp].title}
           </h2>
-          <div className={`flex`}>
+          <div className={`flex justify-center`}>
             <img
               src={ads[popUp].image}
               className={ads[popUp].body ? 'w-32' : 'h-40'}
@@ -73,7 +63,7 @@ const PopupAds = ({ handleClick }) => {
             {ads[popUp].body && <p className="pl-2">{ads[popUp].body}</p>}
           </div>
           <button
-            className={`${ads[popUp].buttonColor} rounded p-1 m-2 justify-center`}
+            className={`${ads[popUp].buttonColor} text-white shadow p-1 m-2 justify-center`}
             onClick={handlePopUp}
           >
             {ads[popUp].buttonText}
@@ -109,7 +99,7 @@ const PopupAds = ({ handleClick }) => {
           </button>
         }
 
-        <button className="px-2 py-1 rounded bg-blue-500 opacity-80 text-white ml-2">
+        <button onClick={() => handleClick('Yes')} className="yes-button ml-2">
           Yes
         </button>
       </div>
